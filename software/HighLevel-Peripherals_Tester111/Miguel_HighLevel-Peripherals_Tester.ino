179b85229be4e0e1c3f50e30bc532c39acf35e50
guantamanera <32084394+guantamanera@users.noreply.github.com>
2018-04-25 19:04:29 -0700

    footprints
    foot print updates

---
+++ software/HighLevel-Peripherals_Tester111/Miguel_HighLevel-Peripherals_Tester.ino
@@ -0,0 +1,402 @@
+#include <Adafruit_L3GD20.h>
+#include <Adafruit_LSM303.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_GPS.h>
+
+
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+
+
+
+//For SD chip
+//const int chipSelect = 10;
+
+//   GPS for Arduino DUE Test
+//   GPS TX to Arduino Due Serial1 RX pin 15
+//   GPS RX to Arduino Due Serial1 TX pin 14
+#define mySerial Serial3
+
+//Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences.
+#define GPSECHO  true
+
+// this keeps track of whether we're using the interrupt
+// off by default!
+boolean usingInterrupt = false;
+
+//For Compass
+Adafruit_LSM303 lsm;
+
+/**
+ * For Gyro Sensor 
+ */
+// Comment this next line to use SPI
+#define USE_I2C
+
+#ifdef USE_I2C
+// The default constructor uses I2C
+Adafruit_L3GD20 gyro;
+#else
+// To use SPI, you have to define the pins
+#define GYRO_CS 4 // labeled CS
+#define GYRO_DO 5 // labeled SA0
+#define GYRO_DI 6  // labeled SDA
+#define GYRO_CLK 7 // labeled SCL
+Adafruit_L3GD20 gyro(GYRO_CS, GYRO_DO, GYRO_DI, GYRO_CLK);
+#endif
+
+  float latitude=0;
+  String latitude1="";
+  String longitude="";
+  String gpsSeconds="";
+  String gpsSpeed="";
+/*
+ * Uncomment to test each module indiviually or all together 
+ */
+
+
+ 
+void setup() {
+
+
+  SDSetup();
+ GPSSetup();
+  CompassSetup();
+  GyroSetup();
+
+}
+
+/*
+ * Uncomment to test each module indiviually or all together 
+ */
+void loop() {
+String flag="A";
+while(!GPS.newNMEAreceived()){
+char c = GPS.read();
+  if(GPS.newNMEAreceived()){
+       // GPS.parse(GPS.lastNMEA());
+        latitude=GPS.latitude;
+        latitude1=GPS.latitude;
+        gpsSeconds=GPS.seconds;
+        longitude=GPS.longitude;
+        gpsSpeed=GPS.speed;
+        flag=gpsSeconds;
+        Serial.print("latxx ");Serial.println(GPS.latitude);
+        Serial.print("Secs ");Serial.println(GPS.seconds);
+          if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+       return;  // we can fail to parse a sentence in which case we should just wait for another
+      }
+      if(flag==gpsSeconds)
+      break;
+}
+
+
+/*
+      // TestGPS()
+      if(!GPS.newNMEAreceived()){
+        TestGyro();
+        delay(1000);
+       TestSDChip();
+       delay(1000);
+       TestCompass();
+       delay(1000);
+       Serial.print("lat :");Serial.println(latitude);
+       Serial.print("lat1 :");Serial.println(latitude1);
+       Serial.print("lat2 :");Serial.println(GPS.latitude);
+      }
+      */
+      
+    
+      
+      
+       Serial.print("lat1 ");Serial.println(latitude);
+       Serial.print("lat2 ");Serial.println(latitude1);
+        Serial.print("Longitude  ");Serial.println(longitude);
+        Serial.print("Speed ");Serial.println(gpsSpeed);
+       Serial.print("GPS seconds ");Serial.println(gpsSeconds);
+       Serial.print("Testing SDcard "); TestSDChip();
+       delay(1000);
+      Serial.print("Testing Compass "); TestCompass();
+      delay(1000);
+     Serial.print("Testing Gyro "); TestGyro();
+
+   
+//  TestSDChip();
+ // delay(1000);
+
+
+
+
+
+   
+//  TestGPS();
+  //delay(1000);
+  
+ /// TestCompass();
+ // delay(1000);
+  
+//TestGyro();
+//  delay(1000);
+}
+
+
+/*
+   MicroSD Setup
+*/
+void SDSetup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+/**
+   Read and Write to SD Card
+*/
+void TestSDChip() {
+  // make a string for assembling the data to log:
+  String dataString = "";
+
+  // read three sensors and append to the string:
+  for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 2) {
+      dataString += ",";
+    }
+  }
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    // print to the serial port too:
+    Serial.println(dataString);
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+
+}
+/**
+ * Setup for GPS Module 
+ */
+void GPSSetup()  {
+
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(9600);
+  Serial.println("Adafruit GPS library basic test!");
+
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  mySerial.begin(9600);
+
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+//// GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_10HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we don't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  // the nice thing about this code is you can have a timer0 interrupt go off
+  // every 1 millisecond, and read data from the GPS for you. that makes the
+  // loop code a heck of a lot easier!
+
+#ifdef __arm__
+  usingInterrupt = false;  //NOTE - we don't want to use interrupts on the Due
+#else
+  useInterrupt(true);
+#endif
+
+  delay(1000);
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
+}
+
+#ifdef __AVR__
+// Interrupt is called once a millisecond, looks for any new GPS data, and stores it
+SIGNAL(TIMER0_COMPA_vect) {
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+#ifdef UDR0
+  if (GPSECHO)
+    if (c) UDR0 = c;
+  // writing direct to UDR0 is much much faster than Serial.print
+  // but only one character can be written at a time.
+#endif
+}
+
+void useInterrupt(boolean v) {
+  if (v) {
+    // Timer0 is already used for millis() - we'll just interrupt somewhere
+    // in the middle and call the "Compare A" function above
+    OCR0A = 0xAF;
+    TIMSK0 |= _BV(OCIE0A);
+    usingInterrupt = true;
+  } else {
+    // do not call the interrupt function COMPA anymore
+    TIMSK0 &= ~_BV(OCIE0A);
+    usingInterrupt = false;
+  }
+}
+#endif //#ifdef__AVR__
+
+uint32_t timer = millis();
+
+/**
+ * Store and Parse data with the GPS
+ */
+void TestGPS()                     
+{
+  // in case you are not using the interrupt above, you'll
+  // need to 'hand query' the GPS, not suggested :(
+  if (! usingInterrupt) {
+    // read data from the GPS in the 'main loop'
+    char c = GPS.read();
+    // if you want to debug, this is a good time to do it!
+    if (GPSECHO)
+      if (c) Serial.print(c);
+  }
+
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis())  timer = millis();
+
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) { 
+    timer = millis(); // reset the timer
+    /*
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    */
+    if (GPS.fix) {
+      /*
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", "); 
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      */
+     // Serial.print("Location (in degrees, works with Google Maps): ");
+      //Serial.print(GPS.latitudeDegrees, 4);
+     // Serial.print(", "); 
+     // Serial.println(GPS.longitudeDegrees, 4);
+      //Serial.print(GPS.latitude, 4); 
+      Serial.print(GPS.lat);
+      /*
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+      */
+    }
+  }
+}
+
+
+
+/**
+ * Setup for the Compass module 
+ */
+void CompassSetup() {
+#ifndef ESP8266
+  while (!Serial);     // will pause Zero, Leonardo, etc until serial console opens
+#endif
+  Serial.begin(9600);
+
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!lsm.begin())
+  {
+    Serial.println("Oops ... unable to initialize the LSM303. Check your wiring!");
+    while (1);
+  }
+}
+
+/**
+ * Reading Data from the compass 
+ */
+void TestCompass() {
+  lsm.read();
+  Serial.print("AX: "); Serial.print((int)lsm.accelData.x); Serial.print(" ");
+  Serial.print("AY: "); Serial.print((int)lsm.accelData.y); Serial.print(" ");
+  Serial.print("AZ: "); Serial.print((int)lsm.accelData.z); Serial.print(" ");
+  Serial.print("MX: "); Serial.print((int)lsm.magData.x);   Serial.print(" ");
+  Serial.print("MY: "); Serial.print((int)lsm.magData.y);   Serial.print(" ");
+  Serial.print("MZ: "); Serial.println((int)lsm.magData.z); Serial.print(" ");
+  delay(100);
+}
+
+/*
+ * Set up for Gyro Module 
+ */
+void GyroSetup() {
+  Serial.begin(9600);
+
+  // Try to initialise and warn if we couldn't detect the chip
+  if (!gyro.begin(gyro.L3DS20_RANGE_250DPS))
+    //if (!gyro.begin(gyro.L3DS20_RANGE_500DPS))
+    //if (!gyro.begin(gyro.L3DS20_RANGE_2000DPS))
+  {
+    Serial.println("Oops ... unable to initialize the L3GD20. Check your wiring!");
+    while (1);
+  }
+}
+
+/*
+ * Getting the axis point from the gyro 
+ */
+void TestGyro() {
+  gyro.read();
+  Serial.print("X: "); Serial.print((int)gyro.data.x);   Serial.print(" ");
+  Serial.print("Y: "); Serial.print((int)gyro.data.y);   Serial.print(" ");
+  Serial.print("Z: "); Serial.println((int)gyro.data.z); Serial.print(" ");
+  delay(100);
+}
